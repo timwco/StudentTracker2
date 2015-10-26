7ebeffee75aef8a4c1cc09d7a5bde0b7e7dd591c
Tim Whitacre <tim@timw.co>
2015-10-26 13:49:33 -0400

    Updated to pull FA from CDN

--- app/assets/javascripts/controllers/admin_dashboard.controller.js
+++ app/assets/javascripts/controllers/admin_dashboard.controller.js
@@ -3,7 +3,6 @@ angular.module('Tracker')
 .controller('AdminController', function ($scope, GithubService) {
 
   GithubService.getUsersInOrg().then( function (response) {
-    console.log(response);
     $scope.members = response.data;
   });
 
