7528fab4ff545a0869e7c7852220fe365f4a15e6
Tim Whitacre <tim@timw.co>
2015-11-02 15:29:15 -0500

    Merging array of info with fields

--- app/assets/javascripts/controllers/reports.controller.js
+++ app/assets/javascripts/controllers/reports.controller.js
@@ -13,16 +13,34 @@ angular.module('Tracker')
 .controller('ReportsSingle', function ($scope, WufooService, $stateParams) {
 
   var user = $('#current-user').val();
+  var userName = $('#current-user-name').val();
 
   $scope.title = $stateParams.formId;
 
   WufooService.getAllFields($stateParams.formId).then( function (response) {
-    console.log(response);
+    // Set Array of Fields
+    var fields = response.data;
     
     WufooService.getEntries($stateParams.formId, user).then( function (response) {
+
+      // Check for Errors
       if (response.data.error) { return $scope.error = response.data.error; }
-      $scope.entries = response.data;
-      console.log(response.data);
+
+      // Get Specific User from Array
+      var report = _.filter(response.data, function (y) {
+        return _.contains(_.values(y), userName + ' - ' + user);
+      });
+
+      // Map a new array of objects with right field name and correct entry
+      var final = [];
+      _.each(fields, function (field) {
+        var obj = {
+          field: field.Title,
+          response: report[0][field.ID]
+        };
+        final.push(obj);
+      });
+      $scope.entries = final;
     });
 
   });
